78cfd65c4b2e3737ba25b12bc4b71067cb7d7538
Zan Markan <zan.markan@gmail.com>
2017-05-30 17:18:42 +0100

    Split out components, remove Feeds
    changed target to pusher platform
    
    Clean up the scripts, remove feeds.ts

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: {
+    main: './src/app.ts',
+  },
+  output: {
+    filename: "target/pusher-platform.js",
+    libraryTarget: "umd",
+    library: "PusherPlatform"
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+        rules: [
+        {
+            test: /\.tsx?$/,
+            loader: 'ts-loader?' + JSON.stringify({ logInfoToStdOut: true }),
+            exclude: /node_modules/,
+        }
+    ]
+    }
+};
