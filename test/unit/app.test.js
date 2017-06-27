cc1b2a83e9cfd6dee3ec7215760321ea1b98c651
minaorangina <mina@pusher.com>
2017-06-27 11:19:39 +0100

    Catches for missing serviceId in App options

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -1,8 +1,15 @@
 const PusherPlatform = require('../../target/pusher-platform.js').default;
 
 describe('App', () => {
-    
+
     test('empty', () => {
         //No tests yet :(
     }) ;
+
+    it('Throws an error if `serviceId` is missing', () => {
+
+      expect(
+        () => new PusherPlatform.App({})
+      ).toThrow(new Error('Expected `serviceId` property in App options'));
+    })
 });
