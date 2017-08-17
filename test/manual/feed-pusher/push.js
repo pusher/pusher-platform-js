2006233ccf5b5fb6f4eee62177f06f32b422112b
Zan Markan <zan.markan@gmail.com>
2017-08-17 15:50:09 +0100

    add script to push to feeds

---
+++ test/manual/feed-pusher/push.js
@@ -0,0 +1,13 @@
+let config = require('../../../config.js');
+let Feeds = require('pusher-feeds-server');
+
+console.log(config);
+
+const feeds = new Feeds({instanceId: config.instanceId, key: config.key});
+
+feeds.publish("my-feed", { name: "Harry Potter"})
+    .then(() => console.log("Publish successful"))
+    .catch((error) => console.log(error));
+
+
+//TODO: expected `instance` property in instance options -> should be instanceId
