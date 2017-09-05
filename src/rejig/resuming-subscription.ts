df59dfae54395187f17c8a095ca8a7332fa9f047
Zan Markan <zan.markan@gmail.com>
2017-09-05 15:16:45 +0100

    Complete mess

--- src/rejig/resuming-subscription.ts
+++ src/rejig/resuming-subscription.ts
@@ -1,4 +1,4 @@
-import { createRetryStrategyOptionsOrDefault, Subscription, SubscriptionState, SubscribeStrategy, RetryStrategyOptions } from './rejig';
+import { createRetryStrategyOptionsOrDefault, Subscription, SubscriptionState, SubscribeStrategy, RetryStrategyOptions, SubscriptionStateTransition } from './rejig';
 import { RetryResolution } from '../retry-strategy/exponential-backoff-retry-strategy';
 import { RetryStrategyResult, Retry } from '../retry-strategy/retry-strategy';
 import { SubscriptionEvent } from '../subscription/base-subscription';
@@ -10,9 +10,17 @@ export let createResumingStrategy: (retryingOptions: RetryStrategyOptions, initi
     retryOptions = createRetryStrategyOptionsOrDefault(retryOptions);
     let retryResolution = new RetryResolution(retryOptions);
 
-    class ResumingSubscription implements Subscription, SubscriptionStateTransition{
+    class ResumingSubscription implements Subscription{
 
         private state: SubscriptionState;
+
+        onTransition(newState: SubscriptionState){
+            this.state = newState;
+        }
+        
+        unsubscribe(){
+            this.state.unsubscribe();
+        }
 
         constructor(
             onOpen, 
@@ -21,49 +29,142 @@ export let createResumingStrategy: (retryingOptions: RetryStrategyOptions, initi
             headers, 
             constructor
         ){
-            let executeStrategy: (lastEventId?: string) => Subscription = (lastEventId) => {
-                
-                if(lastEventId){
-                    headers["Last-Event-Id"] = lastEventId;
+
+            class OpeningSubscriptionState implements SubscriptionState {
+
+                private lastEventId: string = initialEventId;
+                private underlyingSubscription: Subscription;
+
+                constructor(private onTransition: (state: SubscriptionState) => void){
+                    this.underlyingSubscription = nextSubscribeStrategy(
+                        headers => {
+                            onTransition(new OpenSubscriptionState(this.underlyingSubscription, onTransition))
+                        },
+                        error => {
+                            onTransition(new ResumingSubscriptionState(error, this.lastEventId, onTransition));
+                        },
+                        event => {
+                            this.lastEventId = (event as SubscriptionEvent).eventId;
+                            onEvent(event);
+                        },
+                        headers,
+                        constructor
+                    );
                 }
                 
-                let resolveError: (error: any) => RetryStrategyResult =(error) => {
-                    return retryResolution.attemptRetry(error);
-                }
-            
-                let executeStrategyWithLastEventId = () => executeStrategy(lastEventId);
-                
-                return nextSubscribeStrategy(
-                    onOpen,
-                    error => {
+              unsubscribe() {
+                this.underlyingSubscription.unsubscribe();
+                this.onTransition(new EndedSubscriptionState());
+              }
+            }
+            class OpenSubscriptionState implements SubscriptionState {
+                constructor(private subscription: Subscription, private onTransition: (state: SubscriptionState) => void){}
+
+                unsubscribe() {
+                    this.subscription.unsubscribe();
+                    this.onTransition(new EndedSubscriptionState());
+                  }
+            }
+
+            class ResumingSubscriptionState implements SubscriptionState {
+
+                private timeout: number;
+                // private underlyingSubscription: Subscription;                
+
+                constructor(error: any, lastEventId: string, private onTransition: (newState: SubscriptionState) => 
+                void){
+
+                    let executeSubscriptionOnce = (lastEventId: string) => {
+                        let resolveError: (error: any) => RetryStrategyResult =(error) => {
+                            return retryResolution.attemptRetry(error);
+                        }
+    
                         let errorResolution = resolveError(error);
                         if(errorResolution instanceof Retry){
-                            window.setTimeout( executeStrategyWithLastEventId, errorResolution.waitTimeMillis); //TODO:
+                            this.timeout = window.setTimeout(() => {executeNextSubscribeStrategy(lastEventId)}, errorResolution.waitTimeMillis);
                         }
                         else{
-                            onError(error);
+                            onTransition(new FailedSubscriptionState(error));
                         }
-                    },
-                    event => {
-                        lastEventId = (event as SubscriptionEvent).eventId;
-                        onEvent(event);
-                    },
-                    headers,
-                    constructor
-                );
-                }
-            executeStrategy(initialEventId);    
-        }   
+                    }
+                
+                    let executeNextSubscribeStrategy = (lastEventId: string) => {
+                        
+                        if(lastEventId){
+                            headers["Last-Event-Id"] = lastEventId;
+                        }
 
-        onTransition(newState: SubscriptionState){
-            this.state = newState;
-        }
-        
-        unsubscribe(){
-            this.state.unsubscribe();
-            throw new Error("Not implemented");
-        }
+                        let underlyingSubscription = nextSubscribeStrategy(
+                            headers => {
+                                onTransition(new OpenSubscriptionState(underlyingSubscription, onTransition));
+                            },
+                            error => {
+                                executeSubscriptionOnce(lastEventId);
+                            },
+                            event => {
+                                lastEventId = lastEventId;
+                                onEvent(event);
+                            },
+                            headers,
+                            constructor
+                        )
+                    }
+                }
+
+                unsubscribe() {
+                    window.clearTimeout(this.timeout);
+                    this.onTransition(new EndedSubscriptionState());
+                  }
+            }
+
+            class EndedSubscriptionState implements SubscriptionState{
+                unsubscribe() {
+                    throw new Error("Subscription has already ended");
+                  }
+            }
+
+            class FailedSubscriptionState implements SubscriptionState {
+                constructor(error: any){
+                    onError(error);
+                }
+                unsubscribe() {
+                    throw new Error("Subscription has already ended");
+                  }
+            }
+
+        //     let executeStrategy: (lastEventId?: string) => Subscription = (lastEventId) => {
+                
+
+                
+        //         let resolveError: (error: any) => RetryStrategyResult =(error) => {
+        //             return retryResolution.attemptRetry(error);
+        //         }
+            
+        //         let executeStrategyWithLastEventId = () => executeStrategy(lastEventId);
+                
+        //         return nextSubscribeStrategy(
+        //             onOpen,
+        //             error => {
+        //                 let errorResolution = resolveError(error);
+        //                 if(errorResolution instanceof Retry){
+        //                     window.setTimeout( executeStrategyWithLastEventId, errorResolution.waitTimeMillis); //TODO:
+        //                 }
+        //                 else{
+        //                     onError(error);
+        //                 }
+        //             },
+        //             event => {
+        //                 lastEventId = (event as SubscriptionEvent).eventId;
+        //                 onEvent(event);
+        //             },
+        //             headers,
+        //             constructor
+        //         );
+        //         }
+        //     executeStrategy(initialEventId);    
+        // }
     }
+}
 
 
     let strategy: SubscribeStrategy = (onOpen, onError, onEvent, headers, constructor) => {
