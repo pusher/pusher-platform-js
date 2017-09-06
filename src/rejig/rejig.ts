a4f3bb022421e4b8d9fa188d8ebb1e901856e3d3
Zan Markan <zan.markan@gmail.com>
2017-09-06 16:23:33 +0100

    Designing a better cancelable promise and token provider interface

--- src/rejig/rejig.ts
+++ src/rejig/rejig.ts
@@ -1,19 +1,34 @@
+import { TokenProvider } from '../../declarations/token-provider';
 import {Headers, ErrorResponse} from '../base-client';
 import {RetryStrategyResult, Retry} from '../retry-strategy/retry-strategy';
 import {Logger} from '../logger';
-import { BaseSubscription, SubscriptionEvent } from '../subscription/base-subscription';
+import { BaseSubscription } from '../subscription/base-subscription';
 import { RetryResolution } from "../retry-strategy/exponential-backoff-retry-strategy";
 import { createResumingStrategy } from './resuming-subscription';
 import { SubscriptionConstructor, Subscription } from './subscription';
+import { createRetryingStrategy } from './retrying-subscription';
+import { createTokenProvidingStrategy } from './token-providing-subscription';
 
 
+interface CancellablePromise<T> {
+    then(onResult: (result: T) => void);
+    catch(onError: (error: any) => void);
+    cancel();
+}
 
+interface TokenPromise implements CancellablePromise<string> {}
+
+interface TokenProvider {
+    fetchToken(tokenParams?: any): TokenPromise;
+    clearToken(token?: string);
+}
 
 export class H2TransportSubscription implements Subscription {
     unsubscribe(){
         throw new Error("Not implemented");
     }
 }
+
 
 
 
@@ -70,64 +85,6 @@ class FakeClient {
         );
     }
 
-    subscribeResumable(
-        path, headers, listeners, retryStrategyOptions, initialEventId, tokenProvider
-     ): Subscription {
-         
-        let xhrFactory = this.xhrConstructor(path);
-
-        let subscriptionConstructor: SubscriptionConstructor = (headers, onOpen, onError, onEvent) => {
-            return new BaseSubscription(xhrFactory(headers), this.logger, onOpen, onError, onEvent);
-        };
-
-        let resumingSubscriptionStrategy = createResumingStrategy(
-            retryStrategyOptions, 
-            initialEventId,
-            createTokenProvidingStrategy(
-                tokenProvider,
-                createH2TransportStrategy()
-            )
-        );
-
-        return resumingSubscriptionStrategy(
-            listeners.onOpen,
-            listeners.onError,
-            listeners.onEvent,
-            headers,
-            subscriptionConstructor);
-    }
-}
-
-/**
- * Configuration for the retry strategy backoff. Defaults to indefinite retries doubling each time with the max backoff of 5s. First retry is after 1s.
- * Used for creating both resuming and non-resuming (just retrying) subs
- */
-export interface RetryStrategyOptions {
-    initialTimeoutMillis: number,    
-    maxTimeoutMillis: number,
-    limit: number,
-    increaseTimeout: (timeout: number) => number,
-}
-
-export let createRetryStrategyOptionsOrDefault: (RetryStrategyOptions) => RetryStrategyOptions = (options: RetryStrategyOptions) => {
-
-    let initialTimeoutMillis = 1000;
-    let maxTimeoutMillis = 5000;
-    let limit = -1;
-    let increaseTimeoutFunction = (timeout: number) => 2 * timeout <= this.maxTimeoutMillis ? 2*timeout : this.maxTimeoutMillis; 
-
-    if(options.initialTimeoutMillis) initialTimeoutMillis = options.initialTimeoutMillis;
-    if(options.maxTimeoutMillis) maxTimeoutMillis = options.maxTimeoutMillis;
-    if(options.limit) limit = options.limit;
-    if(options.increaseTimeout) increaseTimeoutFunction = options.increaseTimeout;
-
-    return {
-        initialTimeoutMillis: initialTimeoutMillis,
-        maxTimeoutMillis: maxTimeoutMillis,
-        limit: limit,
-        increaseTimeout: increaseTimeoutFunction
-    }
-}
 
 
 //This is a dummy that just executes whatever the constructor does. Doesn't do anything else.
--- src/rejig/token-providing-subscription.ts
+++ src/rejig/token-providing-subscription.ts
@@ -1,10 +1,48 @@
+import { TokenProvider } from '../../declarations/token-provider';
 import { ErrorResponse } from '../base-client';
 import { SubscribeStrategy, Subscription, SubscriptionState } from './subscription';
 
 export interface AsynchronousTokenProvider {
-    fetchToken(onToken: (token: string) => void, onError: (error: any) => void); //Maybe make return a TokenRequest that we can cancel??? Encapsulated promises? 
+    fetchToken(resolve: (token: string) => void, reject: (error: any) => void, tokenParams?: any): NetworkRequest //Maybe make return a TokenRequest that we can cancel??? Encapsulated promises? 
     clearToken(token?: string);
-    cancelFetch(): void; //Go to the token request object
+    // cancelFetch(): void; //Go to the token request object
+    // fetchToken2(tokenParams: any): NetworkRequest;
+}
+
+export interface TokenProvider {
+    fetchToken(resolve: (token: string) => void, reject: (error: any) => void, tokenParams?: any): NetworkRequest
+    clearToken(token?: string);
+}
+
+export interface NetworkRequest{
+    cancel();
+}
+
+let fetchToken: (resolve: (token: string) => void, reject: (error: any) => void, tokenParams?) => NetworkRequest = (resolve, reject, tokenParams) => {
+
+    let xhr: XMLHttpRequest;
+    //Add params to the XHR
+    this.xhr.open;
+    this.xhr.onreadystatechange = event => {
+
+    if(this.xhr.readyState === 4){
+        if (this.xhr.status === 200){
+            resolve(this.xhr.response as string);
+        }
+        else{
+            reject(new ErrorResponse(this.xhr.status, null, this.xhr.responseText));
+        }
+    }
+    };
+    this.xhr.send()
+    
+    let request = {
+        cancel(){
+            xhr.abort();
+        }
+    };
+
+    return request;
 }
 
 export let createTokenProvidingStrategy: (tokenProvider: AsynchronousTokenProvider, nextSubscribeStrategy: SubscribeStrategy) => SubscribeStrategy = (tokenProvider, nextSubscribeStrategy) => {
