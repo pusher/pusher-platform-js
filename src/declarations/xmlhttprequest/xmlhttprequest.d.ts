15679a3c764a6842de403b3b144050915b43d76f
Hamilton Chapman <hamchapman@gmail.com>
2017-11-14 14:32:03 +0000

    Make xmlhttprequest and websocket declarations more accurate

--- src/declarations/xmlhttprequest/xmlhttprequest.d.ts
+++ src/declarations/xmlhttprequest/xmlhttprequest.d.ts
@@ -1,6 +1,6 @@
 declare module 'xmlhttprequest' {
   export class XMLHttpRequest {
-    open(method: string, url: string, async: boolean);
+    open(method: string, url: string, async: boolean): void;
     send(payload?: any): Function;
     setRequestHeader(key: string, value: string): void;
     getAllResponseHeaders(): string;
