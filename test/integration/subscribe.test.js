5e1d39d76df59fc1c34a0b7312324a499ff0231f
Hamilton Chapman <hamchapman@gmail.com>
2018-12-07 16:15:49 +0000

    Add a new test for when the websocket connection gets killed

--- test/integration/subscribe.test.js
+++ test/integration/subscribe.test.js
@@ -7,6 +7,8 @@ const PATH_3_AND_OPEN = 'subscribe_3_continuous';
 const PATH_0_EOS = 'subscribe_0_eos';
 
 describe('Instance Subscribe', () => {
+  var originalTimeout;
+
   beforeEach(() => {
     let logger = new PusherPlatform.ConsoleLogger(1);
 
@@ -26,7 +28,15 @@ describe('Instance Subscribe', () => {
     endCount = 0;
     errorCount = 0;
     errorThrown = false;
+
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
   });
+
+  afterEach(function() {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  });
+
 
   it('subscribes and terminates on EOS after receiving all events', done => {
     instance.subscribeNonResuming({
@@ -68,6 +78,25 @@ describe('Instance Subscribe', () => {
     });
   });
 
+  it('subscribes with resuming subscription but retry strategy that says 0 retries, terminates on EOS, and triggers onEnd callback exactly once', done => {
+    instance.subscribeNonResuming({
+      path: PATH_10_AND_EOS,
+      retryStrategyOptions: neverRetryOptions,
+      listeners: {
+        onOpen: headers => {},
+        onEvent: event => {},
+        onEnd: () => {
+          endCount += 1;
+          expect(endCount).toBe(1);
+          done();
+        },
+        onError: err => {
+          fail();
+        },
+      },
+    });
+  });
+
   it('subscribes to a subscription that is kept open', done => {
     let sub = instance.subscribeNonResuming({
       path: PATH_3_AND_OPEN,
@@ -79,12 +108,15 @@ describe('Instance Subscribe', () => {
           if (eventCount > 3) {
             fail(`Too many events received: ${eventCount}`);
           }
-          if (eventCount == 3) {
+          if (eventCount === 3) {
+            sub.unsubscribe();
             done();
           }
         },
         onEnd: () => {
-          fail("onEnd triggered. This shouldn't be!");
+          if (eventCount !== 3) {
+            fail("onEnd triggered. This shouldn't be!");
+          }
         },
         onError: err => {
           fail("onError triggered - this shouldn't be!");
@@ -104,7 +136,7 @@ describe('Instance Subscribe', () => {
           if (eventCount > 3) {
             fail(`Too many events received: ${eventCount}`);
           }
-          if (eventCount == 3) {
+          if (eventCount === 3) {
             sub.unsubscribe();
           }
         },
@@ -137,7 +169,7 @@ describe('Instance Subscribe', () => {
     });
   });
 
-  it('subsccribes and receives EOS with retry-after headers', done => {
+  it('subscribes and receives EOS with retry-after headers', done => {
     let sub = instance.subscribeNonResuming({
       path: 'subscribe_retry_after',
       retryStrategyOptions: neverRetryOptions,
@@ -148,11 +180,44 @@ describe('Instance Subscribe', () => {
         },
         onEnd: () => {
           fail('We should get an error');
-          done();
         },
         onError: err => {
           expect(err.headers['Retry-After']).toBe('10');
           done();
+        },
+      },
+    });
+  });
+
+  it('subscribes and receives a close message with retry-after headers', done => {
+    let sub = instance.subscribeResuming({
+      path: 'subscribe_connection_close',
+      listeners: {
+        onOpen: headers => {},
+        onEvent: event => {
+          eventCount += 1;
+
+          switch (eventCount) {
+            case 1:
+              expect(event.body.test_id).toBe(99);
+              // Send a message to request the websocket connection be closed by
+              // the server
+              instance.client.websocketTransport.sendMessage([999, 123]);
+              break;
+            case 2:
+              expect(event.body.test_id).toBe(100);
+              done();
+              break;
+            default:
+              fail('Received too many events from the server')
+              break;
+          }
+        },
+        onEnd: () => {
+          fail('We should get an error that is handled internally and then close ourselves');
+        },
+        onError: err => {
+          fail('We should not see an error - it should be handled internally because the subscription is resuming')
         },
       },
     });
